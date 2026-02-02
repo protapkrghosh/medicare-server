10afcc6760c4ede316dd6262110e351cb5e54711
protapkrghosh <protapg518@gmail.com>
2026-02-02 19:46:56 +0600

    fixed data insert issue

--- prisma/migrations/20260202122244_init/migration.sql
+++ prisma/migrations/20260202122244_init/migration.sql
@@ -8,6 +8,18 @@ CREATE TYPE "PaymentStatus" AS ENUM ('CASH_ON_DELIVERY', 'MOBILE_BANKING', 'BANK
 CREATE TYPE "CommentStatus" AS ENUM ('APPROVED', 'REJECT');
 
 -- CreateTable
+CREATE TABLE "TestUsers" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "TestUsers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Category" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -19,13 +31,15 @@ CREATE TABLE "Category" (
 -- CreateTable
 CREATE TABLE "Medicines" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
     "description" TEXT NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
-    "stock" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "stock" INTEGER NOT NULL DEFAULT 0,
     "image" TEXT NOT NULL,
     "manufacturer" TEXT NOT NULL,
     "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "authorId" TEXT,
+    "categoryId" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -63,7 +77,16 @@ CREATE TABLE "Review" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "TestUsers_email_key" ON "TestUsers"("email");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "Medicines" ADD CONSTRAINT "Medicines_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_medicineId_fkey" FOREIGN KEY ("medicineId") REFERENCES "Medicines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Review" ADD CONSTRAINT "Review_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Review"("id") ON DELETE CASCADE ON UPDATE CASCADE;
