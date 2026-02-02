867d47738f03ee49705b2cc969677af5986c7a4e
protapkrghosh <protapg518@gmail.com>
2026-02-02 20:50:19 +0600

    added authentication

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
+import { auth } from "./lib/auth";
+import { toNodeHandler } from "better-auth/node";
 import { medicineRouter } from "./modules/medicine/medicine.route";
 
 const app = express();
 
-app.use(express.json());
 app.use(
    cors({
       origin: process.env.APP_URL || "http://localhost:3000",
       credentials: true,
    }),
 );
+app.use(express.json());
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use("/api", medicineRouter);
 
