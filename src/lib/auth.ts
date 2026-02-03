12ea41fd0ce9378b988dd3e1ad204798c73d7f43
protapkrghosh <protapg518@gmail.com>
2026-02-03 16:06:00 +0600

    added social login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -29,4 +29,13 @@ export const auth = betterAuth({
       enabled: true,
       autoSignIn: false,
    },
+
+   socialProviders: {
+      google: {
+         clientId: process.env.GOOGLE_CLIENT_ID as string,
+         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+         accessType: "offline",
+         prompt: "select_account consent",
+      },
+   },
 });
